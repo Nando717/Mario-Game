e5ce80af208ccbaff370dee87c1077774c144a83
Iverlandio Edson <iverlandioedsonn@gmail.com>
2023-09-17 13:10:38 -0300

    fish project, add new behaviors and visuals

--- index.js
+++ index.js
@@ -14,9 +14,19 @@ mario.classList.remove("jump")
 const loop = setInterval(()=> {
 
 const pipePosition = pipe.offsetLeft;
-if ( pipePosition <= 120){
+const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
+if ( pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
     pipe.style.animation = "none";
     pipe.style.left = `${pipePosition}px `
+
+    mario.style.animation = "none";
+    mario.style.bottom = `${marioPosition}px `
+
+    mario.src = "./Imagens/game-over.png";
+    mario.style.width = '75px'
+    mario.style.marginleft = "50px"
+
+    clearInterval(loop);
 }
 }, 10)
 
